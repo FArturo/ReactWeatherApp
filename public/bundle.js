98486a935a7e9c9a243c0b004b2921791f32e75b
Fernando Arias <FernandoArias92@gmail.com>
2017-01-13 01:29:49 -0500

    centered all the content by adding styling

--- public/bundle.js
+++ public/bundle.js
@@ -24923,11 +24923,14 @@
 	      null,
 	      React.createElement(Nav, null),
 	      React.createElement(
-	        'h2',
-	        null,
-	        'Main Component'
-	      ),
-	      this.props.children
+	        'div',
+	        { className: 'row' },
+	        React.createElement(
+	          'div',
+	          { className: 'columns medium-6 large-4 small-centered' },
+	          this.props.children
+	        )
+	      )
 	    );
 	  }
 	});
